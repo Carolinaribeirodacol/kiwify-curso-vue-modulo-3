9b00144a47ce352d891810f9d4ec013b41b54443
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2025-12-14 22:40:39 -0300

    feat: Preparando ambiente para o módulo - routes

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="p-10">
+    <button class="btn btn-primary" @click="handleAbout()">about</button>
+  </main>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function handleAbout() {
+  router.push('/about')
+}
+</script>
